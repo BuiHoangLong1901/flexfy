4ed999e231206409f6d7f5354009f102ff599f93
Hoang Long <longbh@meta-technology.com.vn>
2023-08-23 17:35:27 +0700

    deploy

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ import { handler as ssrHandler } from "./dist/server/entry.mjs";
 const app = express();
 app.use(compression());
 
-app.use(express.static("dist/client", { maxAge: "7d" }));
-app.use((req, res, next) => {
-  ssrHandler(req, res, next);
-});
-
+app.use(express.static("./dist/client", { maxAge: "7d" }));
+app.use(ssrHandler);
+console.log("Connect success!!!!");
 app.listen(3000, "0.0.0.0", () => {
   console.log("Application is running at http://127.0.0.1:3000");
 });

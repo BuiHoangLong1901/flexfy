2b6a9a336a3950aa0ca4020ad1b679d20f3a5e85
Hoang Long <longbh@meta-technology.com.vn>
2023-08-24 17:14:34 +0700

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,38 @@
+import node from "@astrojs/node";
 import react from "@astrojs/react";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "astro-sitemap";
 import { defineConfig } from "astro/config";
 import dotenv from "dotenv";
+import compress from "astro-compress";
 dotenv.config();
+
 const site = process.env.SITE || "https://flexfy.meta-book.online";
-// astro.config.mjs
-import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site,
-  integrations: [react()],
+  integrations: [
+    sitemap({
+      customPages: [`${site}`],
+      lastmod: new Date(),
+    }),
+    robotsTxt({
+      policy: [
+        {
+          userAgent: "*",
+          allow: ["/"],
+          crawlDelay: 10,
+        },
+      ],
+    }),
+    react(),
+    compress({
+      SVG: false,
+    }),
+  ],
   output: "server",
-  adapter: netlify(),
-  vite: {
-    ssr: {
-      noExternal: ["path-to-regexp"],
-    },
-  },
+  adapter: node({ mode: "middleware" }),
+  vite: { ssr: { noExternal: ["path-to-regexp"] } },
   compressHTML: true,
 });
---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import compression from "compression";
+import { handler as ssrHandler } from "./dist/server/entry.mjs";
+
+const app = express();
+app.use(compression());
+
+app.use(express.static("./dist/client", { maxAge: "7d" }));
+app.use(ssrHandler);
+console.log("Connect success!!!!");
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Application is running at http://127.0.0.1:3000");
+});

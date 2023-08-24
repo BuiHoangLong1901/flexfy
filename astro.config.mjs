8a5fb626228dd3468dcf8d5419d6c104c5f74d76
Hoang Long <longbh@meta-technology.com.vn>
2023-08-24 17:06:10 +0700

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
+import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
+import dotenv from "dotenv";
+dotenv.config();
+const site = process.env.SITE || "https://flexfy.meta-book.online";
 // astro.config.mjs
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
+  site,
+  integrations: [react()],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  vite: {
+    ssr: {
+      noExternal: ["path-to-regexp"],
+    },
+  },
+  compressHTML: true,
 });

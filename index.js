05fa2c0ffa4bf2f6ef8ee2bdef1dd4cddad08d79
Hoang Long <longbh@meta-technology.com.vn>
2023-08-25 09:04:28 +0700

    up

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 import express from "express";
-import compression from "compression";
 import { handler as ssrHandler } from "./dist/server/entry.mjs";
 
 const app = express();
-app.use(compression());
-
 app.use(express.static("./dist/client", { maxAge: "7d" }));
 app.use(ssrHandler);
 console.log("Connect success!!!!");
